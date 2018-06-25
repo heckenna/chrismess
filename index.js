a329a728fc08e024e3762385fbd831c5d41c5efd
heckenna <heckenna@rose-hulman.edu>
2018-06-25 17:16:34 -0400

    Bonus Credit Complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('It works!');
 const button = document.querySelector('button');
 
 const updateText = function() {
-    const text = document.querySelector('h1');
+    const text = document.querySelector('#heading');
     text.textContent = "What do we have here?";
 }
 
