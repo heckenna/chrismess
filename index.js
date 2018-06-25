45ee4016ea3deef597f0ede2fbbf52d87640790a
heckenna <heckenna@rose-hulman.edu>
2018-06-25 17:14:00 -0400

    Homework

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('It works!');
+
+const button = document.querySelector('button');
+
+const updateText = function() {
+    const text = document.querySelector('h1');
+    text.textContent = "What do we have here?";
+}
+
+button.addEventListener('click', updateText);
