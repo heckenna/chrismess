b67c6ec027f18cbd017b4505c266f0c16d6cc905
heckenna <heckenna@rose-hulman.edu>
2018-06-26 09:04:03 -0400

    Super Bonus  actuallyh done.  Ignore the commit prior to this.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 console.log("It works!");
 
 const button = document.querySelector("button");
-const otherButton = document.querySelector("#sub")
+const otherButton = document.querySelector("#sub");
+const key = "Enter".key;
 
 const updateText = function() {
     const text = document.querySelector("#heading");
@@ -11,6 +12,10 @@ const updateFirst = function() {
     const head = document.querySelector("h1");
     const box = document.querySelector("#textHeading").value;
     head.textContent = box;
+    event.preventDefault();
 }
+
+
 button.addEventListener("click", updateText);
 otherButton.addEventListener("click", updateFirst);
+key.addEventListener("keyup", updateFirst);
