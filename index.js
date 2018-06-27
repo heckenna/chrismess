99ac55800743bd8e5603dffd615828b8544ba2a9
heckenna <heckenna@rose-hulman.edu>
2018-06-27 11:02:51 -0400

    Do the Super Mega Bonus Credit

--- index.js
+++ index.js
@@ -7,25 +7,34 @@ const updateText = function() {
     const text = document.querySelector("#heading");
     text.textContent = "What do we have here?";
 }
+
+const prepListElement = function(flickBox, yearBox) {
+    listElement = document.createElement('li');
+
+    const flickName = document.createElement("span");
+    flickName.textContent = flickBox.value + " ";
+    flickName.style.color = "red";
+    flickName.style.fontSize = "50pt";
+
+    const flickYear = document.createElement("span");
+    flickYear.textContent = yearBox.value;
+    flickYear.style.fontFamily = "Time New Roman";
+    flickYear.style.fontSize = "40pt";
+
+    listElement.append(flickName);
+    listElement.append(flickYear);
+
+
+    return listElement
+}
+
 const addToList = function(ev) {
     ev.preventDefault();
     const list = document.querySelector("#flicks");
     const flickBox = document.querySelector("#flickName");
     const yearBox = document.querySelector("#flickYear");
 
-    const flickName = document.createElement("span");
-    flickName.textContent = flickBox.value + " ";
-    flickName.style.color = "red";
-    flickName.style.fontSize = "50pt";
-    const flickYear = document.createElement("span");
-    flickYear.textContent = yearBox.value;
-    flickYear.style.fontFamily = "Time New Roman";
-    flickYear.style.fontSize = "40pt";
-
-    const listElement = document.createElement('li');
-    
-    listElement.append(flickName);
-    listElement.append(flickYear);
+    const listElement = prepListElement(flickBox, yearBox);
 
     list.appendChild(listElement);
     // list.innerHTML += `<li>${val}</li>`;
