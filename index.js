f44dfffc695b6c39750cfd037f6a24c5b0707162
heckenna <heckenna@rose-hulman.edu>
2018-06-27 16:32:33 -0400

    Commit what we did in class

--- index.js
+++ index.js
@@ -1,48 +1,104 @@
-console.log("It works!");
+class App {
+    constructor() {
+        const button = document.querySelector("button");
+        const submitButton = document.querySelector("#sub");
 
-const button = document.querySelector("button");
-const submitButton = document.querySelector("#sub");
+        button.addEventListener("click", () => {
+            this.updateText()
+        })
+        submitButton.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            this.addToList(ev);
+            })
+        }
 
-const updateText = function() {
-    const text = document.querySelector("#heading");
-    text.textContent = "What do we have here?";
+    addToList(ev) {
+        ev.preventDefault();
+        const list = document.querySelector("#flicks");
+        const flickBox = document.querySelector("#flickName");
+        const yearBox = document.querySelector("#flickYear");
+        
+        const flick = { 
+            name: flickBox.value,
+            year: yearBox.value,
+            }
+        
+        // const listElement = prepListElement(flickBox, yearBox);
+        
+        list.appendChild(this.renderItem(flick));
+        // list.innerHTML += `<li>${val}</li>`;
+        flickBox.value =  "";
+        yearBox.value = "";
+    }
+
+    prepListElement(flickBox, yearBox) {
+        listElement = document.createElement('li');
+        
+        const flickName = renderProperty("name", flickBox.value + " ");
+        flickName.style.color = "red";
+        flickName.style.fontSize = "50pt";
+        
+        const flickYear = renderProperty("year",  yearBox.value);
+         
+        flickYear.style.fontFamily = "Time New Roman";
+        flickYear.style.fontSize = "40pt";
+        
+        listElement.append(flickName);
+        listElement.append(flickYear);
+        
+        
+         return listElement;
+    }
+
+    renderItem(flick) {
+        const item = document.createElement("li");
+        item.classList.add("flick");
+    
+        const properties = Object.keys(flick);
+    
+        //loop over each properties
+        properties.forEach((propertyName) => {
+            //build  a span
+            const span = this.renderProperty(propertyName, flick[propertyName]);
+            item.appendChild(span);
+        })
+    
+    
+        return item;
+    }
+
+
+    renderProperty(name, value) {
+        const span = document.createElement("span");
+        span.classList.add(name);
+        span.textContent = value + " ";
+        return span;
+    }
+    
+    updateText() {
+        const text = document.querySelector("#heading");
+        text.textContent = "What do we have here?";
+    }
+
+    
+    
+    
+
+    
+
+    
 }
-
-const prepListElement = function(flickBox, yearBox) {
-    listElement = document.createElement('li');
-
-    const flickName = document.createElement("span");
-    flickName.textContent = flickBox.value + " ";
-    flickName.style.color = "red";
-    flickName.style.fontSize = "50pt";
-
-    const flickYear = document.createElement("span");
-    flickYear.textContent = yearBox.value;
-    flickYear.style.fontFamily = "Time New Roman";
-    flickYear.style.fontSize = "40pt";
-
-    listElement.append(flickName);
-    listElement.append(flickYear);
+ const app = new App;
 
 
-    return listElement
-}
-
-const addToList = function(ev) {
-    ev.preventDefault();
-    const list = document.querySelector("#flicks");
-    const flickBox = document.querySelector("#flickName");
-    const yearBox = document.querySelector("#flickYear");
-
-    const listElement = prepListElement(flickBox, yearBox);
-
-    list.appendChild(listElement);
-    // list.innerHTML += `<li>${val}</li>`;
-    flickBox.value =  "";
-    yearBox.value = "";
-}
 
 
-button.addEventListener("click", updateText);
-submitButton.addEventListener("click", addToList);
-//key.addEventListener("keyup", updateFirst);
+
+
+
+
+
+
+
+
+
