6737f0ca8e397993a2925a025ddc8796aef1f4ca
heckenna <heckenna@rose-hulman.edu>
2018-06-27 10:47:18 -0400

    Do the Homework and the Bonus Credit for Day 2

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 console.log("It works!");
 
 const button = document.querySelector("button");
-const otherButton = document.querySelector("#sub");
-//const key = keyCode = 13;
+const submitButton = document.querySelector("#sub");
 
 const updateText = function() {
     const text = document.querySelector("#heading");
     text.textContent = "What do we have here?";
 }
-const updateFirst = function() {
-    const head = document.querySelector("h1");
-    const box = document.querySelector("#textHeading").value;
-    head.textContent = box;
-    event.preventDefault();
+const addToList = function(ev) {
+    ev.preventDefault();
+    const list = document.querySelector("#flicks");
+    const flickBox = document.querySelector("#flickName");
+    const yearBox = document.querySelector("#flickYear");
+
+    const flickName = document.createElement("span");
+    flickName.textContent = flickBox.value + " ";
+    flickName.style.color = "red";
+    flickName.style.fontSize = "50pt";
+    const flickYear = document.createElement("span");
+    flickYear.textContent = yearBox.value;
+    flickYear.style.fontFamily = "Time New Roman";
+    flickYear.style.fontSize = "40pt";
+
+    const listElement = document.createElement('li');
+    
+    listElement.append(flickName);
+    listElement.append(flickYear);
+
+    list.appendChild(listElement);
+    // list.innerHTML += `<li>${val}</li>`;
+    flickBox.value =  "";
+    yearBox.value = "";
 }
 
 
 button.addEventListener("click", updateText);
-otherButton.addEventListener("click", updateFirst);
+submitButton.addEventListener("click", addToList);
 //key.addEventListener("keyup", updateFirst);
